5202258a364706b5c56a7612ed54663b247164e4
ponta10 <mponta2002@gmail.com>
2023-08-08 22:46:57 +0900

    add animation

---
+++ components/animations/SlideUp.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+import { useInView } from "react-intersection-observer";
+import styled from "styled-components";
+
+//TypeScriptの型定義 (今回は無視してOK)
+type fadeAnimationType = {
+  children: ReactNode;
+};
+
+export const SlideUp = (props: fadeAnimationType) => {
+  const { children } = props;
+
+  /**
+   * スクロールイベントのオプション
+   * 「ref」検知する要素
+   * 「inView」要素が見えたかどうか(見えたらtrue)
+   * 「rootMargin」要素の検知の「余白」を設定
+   * 「triggerOnce」検知を一度だけ行うかどうか
+   */
+  const { ref, inView } = useInView({
+    rootMargin: "-100px",
+    triggerOnce: true,
+  });
+
+  return (
+    /**
+     * ★スクロールさせたい要素を囲む
+     * refで指定すると対象の要素になる
+     * inViewのtrueかfalseを受け取り、styled-componentに渡す
+     */
+    <SlideUpFadeElem inView={inView} ref={ref}>
+      {children}
+    </SlideUpFadeElem>
+  );
+};
+
+/**
+ * ★inView(trueかfalse)で受け取り、それに応じてcssを切り替える
+ */
+const SlideUpFadeElem = styled.span<{ inView: boolean }>`
+  display: inline-block;
+  transition:
+    transform 0.6s cubic-bezier(0.47, 0, 0.745, 0.715),
+    opacity 0.6s cubic-bezier(0.47, 0, 0.745, 0.715);
+  transform: translateY(${(props) => (props.inView ? "0" : "40px")});
+  opacity: ${(props) => (props.inView ? 1 : 0)};
+  width: 100%;
+`;
