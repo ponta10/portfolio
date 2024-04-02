8501e7e9140c477d62bc119eb6b0f7ed25b7ea18
ponta10 <mponta2002@gmail.com>
2024-04-02 23:14:03 +0900

    lint修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,9 @@ import { Suspense } from "react";
 import dynamic from "next/dynamic";
 import LoadingScreen from "@/components/atoms/LoadingScreen";
 
-
-const Top = dynamic(() => import('@/components/pages/Top'), {
+const Top = dynamic(() => import("@/components/pages/Top"), {
   loading: () => <LoadingScreen />,
 });
-
 
 export default function Home() {
   return (
