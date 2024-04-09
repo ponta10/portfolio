449907c3cc6838f5a9fb61e93c1964a8cfab9af0
ponta10 <mponta2002@gmail.com>
2024-04-09 16:13:05 +0900

    OGP設定箇所修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,34 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>三浦 広太 | Kota Miura</title>
+        <meta property="og:title" content="三浦 広太 | Kota Miura" />
+        <meta
+          property="og:description"
+          content="学生エンジニア三浦広太のポートフォリオサイトです。"
+        />
+        <meta
+          property="og:image"
+          content="http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn"
+        />
+        <meta
+          property="og:url"
+          content="https://portfolio-eight-gamma-93.vercel.app/"
+        />
+        <meta
+          name="twitter:card"
+          content="http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,26 +38,6 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <title>三浦 広太 | Kota Miura</title>
-          <meta property="og:title" content="三浦 広太 | Kota Miura" />
-          <meta
-            property="og:description"
-            content="学生エンジニア三浦広太のポートフォリオサイトです。"
-          />
-          <meta
-            property="og:image"
-            content="http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn"
-          />
-          <meta
-            property="og:url"
-            content="https://portfolio-eight-gamma-93.vercel.app/"
-          />
-          <meta
-            name="twitter:card"
-            content="http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn"
-          />
-        </Head>
         <body>
           <Main />
           <NextScript />
