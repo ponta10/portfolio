a31062f151a877b1bb75fbf5e722ba998de7489c
ponta10 <mponta2002@gmail.com>
2024-04-09 16:57:54 +0900

    共通OGP設定

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from "next";
 import Document, {
   DocumentContext,
   Head,
@@ -7,33 +6,6 @@ import Document, {
   NextScript,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-
-export const metadata: Metadata = {
-  title: "三浦 広太 | Kota Miura",
-  description: "学生エンジニア三浦広太のポートフォリオサイトです。",
-  openGraph: {
-    title: "三浦 広太 | Kota Miura",
-    description: "学生エンジニア三浦広太のポートフォリオサイトです。",
-    url: "https://portfolio-eight-gamma-93.vercel.app/",
-    siteName: "三浦 広太 | Kota Miura",
-    images: [
-      {
-        url: "http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn",
-        width: 1602,
-        height: 916,
-      },
-    ],
-    locale: "ja_JP",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@site",
-    creator: "@creator",
-    images:
-      "http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn",
-  },
-};
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -66,7 +38,26 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>三浦 広太 | Kota Miura</title>
+          <meta property="og:title" content="三浦 広太 | Kota Miura" />
+          <meta
+            property="og:description"
+            content="学生エンジニア三浦広太のポートフォリオサイトです。"
+          />
+          <meta
+            property="og:image"
+            content="http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn"
+          />
+          <meta
+            property="og:url"
+            content="https://portfolio-eight-gamma-93.vercel.app/"
+          />
+          <meta
+            name="twitter:card"
+            content="http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
