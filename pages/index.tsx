15624bb7bd2b05125162c413b2ff2d7242c4fe43
ponta10 <mponta2002@gmail.com>
2024-04-09 16:41:39 +0900

    ページごとOGP設定

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,30 @@
-import { Suspense } from "react";
-import dynamic from "next/dynamic";
-import LoadingScreen from "@/components/atoms/LoadingScreen";
-
-const Top = dynamic(() => import("@/components/pages/Top"), {
-  loading: () => <LoadingScreen />,
-});
+import Head from "next/head";
+import Top from "@/components/pages/Top";
 
 export default function Home() {
   return (
-    <Suspense fallback={<LoadingScreen />}>
+    <>
+      <Head>
+        <title>三浦 広太 | Kota Miura</title>
+        <meta property="og:title" content="三浦 広太 | Kota Miura" />
+        <meta
+          property="og:description"
+          content="学生エンジニア三浦広太のポートフォリオサイトです。"
+        />
+        <meta
+          property="og:image"
+          content="http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn"
+        />
+        <meta
+          property="og:url"
+          content="https://portfolio-eight-gamma-93.vercel.app/"
+        />
+        <meta
+          name="twitter:card"
+          content="http://drive.google.com/uc?export=view&id=1yeuOBPDMfK4svymhtQUjWh6WuQLcK1Nn"
+        />
+      </Head>
       <Top />
-    </Suspense>
+    </>
   );
 }
